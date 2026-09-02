6902e3f24a5c3835c2c6c385efe3310e53b59b9f
Michael Kwok <kwokm@sas.upenn.edu>
2026-09-01 21:23:27 -0700

    Swap sample properties to OC CRMLS listings with professional photos

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "photos.zillowstatic.com", pathname: "/**" },
       { protocol: "https", hostname: "www.zillowstatic.com", pathname: "/**" },
       { protocol: "https", hostname: "static.zillowstatic.com", pathname: "/**" },
+      { protocol: "https", hostname: "d36xftgacqn2p.cloudfront.net", pathname: "/**" },
     ],
   },
 };
